aab08ea00f3c06bbe90f67a9942f0d2253c5812d
Arthur <108303210+arthurgalluccilima@users.noreply.github.com>
2023-07-29 17:21:55 -0300

    Version 1.0
    Mudei a estrutura do programa. Agora, as operaçoes sao resolvidas pela bibliotec Sympy.

--- app.py
+++ app.py
@@ -1,71 +1,60 @@
 import sympy
 from tkinter import *
-from funcionalidades import *
-
 
 class App(Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.grid_propagate(False)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0,weight=1)
-        self.grid_configure(rowspan=4, columnspan=5)
 
-        self.entrada = Entry(width="20", font="Arial 32")
-        self.entrada.grid(row=1, column=1, padx=50, pady=50)
+        self.entrada = Entry(font="Arial 32")
+        self.entrada.place_configure(x=50, y=50)
 
-        self.resultado = Label(text="1.000.000.000", font="Arial 30")
-        self.resultado.grid(row=1, column=5, padx=50, pady=50)
-
+        self.resultado = Label(text="Bem vindo.", font="Arial 30", border=True, borderwidth=2)
+        self.resultado.place_configure(x=800, y=50)
 
         # row 2
         self.b1 = Button(text="1", font="Arial 25", width=3, height=2, command=lambda: self.click("1"))
-        self.b1.grid(row=2, column=2, padx=5, pady=5)
+        self.b1.place_configure(x=50, y=150)
 
-        self.b2 = Button(text="2", font="Arial 25", width=3, height=2)
-        self.b2.grid(row=2, column=3, padx=5, pady=5)
+        self.b2 = Button(text="2", font="Arial 25", width=3, height=2, command=lambda: self.click("2"))
+        self.b2.place_configure(x=150, y=150)
 
-        self.b3 = Button(text="3", font="Arial 25", width=3, height=2)
-        self.b3.grid(row=2, column=4, padx=5, pady=5)
+        self.b3 = Button(text="3", font="Arial 25", width=3, height=2, command=lambda: self.click("3"))
+        self.b3.place_configure(x=250, y=150)
 
 
         # row 3
-        self.b4 = Button(text="4", font="Arial 25", width=3, height=2)
-        self.b4.grid(row=3, column=2, padx=5, pady=5)
+        self.b4 = Button(text="4", font="Arial 25", width=3, height=2, command=lambda: self.click("4"))
+        self.b4.place_configure(x=50, y=250)
+        
+        self.b5 = Button(text="5", font="Arial 25", width=3, height=2, command=lambda: self.click("5"))
+        self.b5.place_configure(x=150, y=250)
 
-        self.b5 = Button(text="5", font="Arial 25", width=3, height=2)
-        self.b5.grid(row=3, column=3, padx=5, pady=5)
-
-        self.b6 = Button(text="6", font="Arial 25", width=3, height=2)
-        self.b6.grid(row=3, column=4, padx=5, pady=5)
+        self.b6 = Button(text="6", font="Arial 25", width=3, height=2, command=lambda: self.click("6"))
+        self.b6.place_configure(x=250, y=250)
 
 
         # row 4
-        self.b7 = Button(text="7", font="Arial 25", width=3, height=2)
-        self.b7.grid(row=4, column=2, padx=5, pady=5)
+        self.b7 = Button(text="7", font="Arial 25", width=3, height=2, command=lambda: self.click("7"))
+        self.b7.place_configure(x=50, y=350)
 
-        self.b8 = Button(text="8", font="Arial 25", width=3, height=2)
-        self.b8.grid(row=4, column=3, padx=5, pady=5)
+        self.b8 = Button(text="8", font="Arial 25", width=3, height=2, command=lambda: self.click("8"))
+        self.b8.place_configure(x=150, y=350)
 
-        self.b9 = Button(text="9", font="Arial 25", width=3, height=2)
-        self.b9.grid(row=4, column=4, padx=5, pady=5)
+        self.b9 = Button(text="9", font="Arial 25", width=3, height=2, command=lambda: self.click("9"))
+        self.b9.place_configure(x=250, y=350)
 
-        self.resolver = Button(text="resolver", font="Arial 20", command=lambda: self.solucionar())
-        self.resolver.grid(row=4, column=4)
+        self.resolver = Button(text="Resolver", font="Arial 20", width=8, height=2, command=lambda: self.solucionar())
+        self.resolver.place_configure(x=900, y=350)
 
 
     def click(self, n):
-        self.entrada.insert(0, n)
-        print("Added")
+        self.entrada.insert((len(self.entrada.get())), n)
 
     def solucionar(self):
         exp = self.entrada.get()
         x = sympy.symbols("x")
-        exp + "= x"
-        sympy.solve(exp, x)
-        print(x)
-
-
+        self.resultado.configure(text=(sympy.solve(exp, x)))
+                
 root = Tk()
 root.geometry("1100x500")
 window = App(root)
